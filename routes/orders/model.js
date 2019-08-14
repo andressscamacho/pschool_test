fddb7a8e8a95616d7fe080122e4b69d5a736ec0e
Andrés Velandia <andresfcvelandia@gmail.com>
2019-08-14 14:24:18 -0500

    Comments added

--- routes/orders/model.js
+++ routes/orders/model.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
+/*
+    Modelo para pedido
+ */
 const order = mongoose.Schema({
+    //Fecha de realización del pedido
     date: Date,
 }, { versionKey: false });
 
+/*
+    Modelo para detalle de pedido
+ */
 const orderDetail = mongoose.Schema({
+    //Identificador del pedido
     order_id: {type: ObjectId, required: true},
-    quantity: Number,
+    //Cantidad del producto
+    quantity: {type: Number, default: 1},
+    //Identificador del producto
     product_id: {type: ObjectId, required: true}
 }, { versionKey: false });
 
