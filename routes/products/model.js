e4e2617993f9ea28f91acb4c1e7f56e7deccc1a8
Andrés Velandia <andresfcvelandia@gmail.com>
2019-08-14 14:24:41 -0500

    Comments added. Text search indexing for product name

--- routes/products/model.js
+++ routes/products/model.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId;
 
+/*
+    Modelo del producto
+ */
 const product = mongoose.Schema({
+    //Nombre del producto
     name: String,
+    //Precio del producto
     price: Number
 }, { versionKey: false });
+
+//Indexado de nombre para búsquedas de texto
+product.index({name: 'text'});
 
 module.exports = mongoose.model('product', product);
