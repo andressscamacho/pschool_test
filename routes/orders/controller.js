d5a0ab1737d43fcec07684bd8920ecfc9f046a18
Andrés Velandia <andresfcvelandia@gmail.com>
2019-08-14 14:23:21 -0500

    Comments added. Filter support

--- routes/orders/controller.js
+++ routes/orders/controller.js
@@ -4,13 +4,61 @@ const createController = require('../../utils/createController');
 
 const controller = {};
 
+/**
+ * Obtiene los pedidos
+ * @param request Objeto que contiene información para filtrar pedidos
+ *                el objeto puede contener:
+ *                product_ids: array con ids de productos
+ *                minDate: fecha mínima de creación
+ *                maxDate: fecha máxima de creación
+ * @returns Los pedidos
+ */
 controller.get = async (request) => {
-    const orders = await datasources.orders.find({});
+    const requestQuery = request.query;
+    const ordersQuery = {};
+    const orderDetailsQuery = {};
+
+    try {
+        const productIds = JSON.parse(requestQuery.product_ids);
+
+        if (Array.isArray(productIds) &&
+            productIds.length > 0) {
+            orderDetailsQuery.product_id = {
+                $in: productIds
+            };
+        }
+    } catch (e) {
+        //Do nothing
+    }
+
+    if (Object.keys(orderDetailsQuery).length > 0) {
+        const orderDetails = await datasources.orderDetails.find(orderDetailsQuery);
+        const orderIds = orderDetails.map(orderDetail => orderDetail.order_id);
+
+        ordersQuery._id = {
+            $in: orderIds
+        }
+    }
+
+    if (requestQuery.minDate) {
+        ordersQuery.date = {
+            $gte: Date(requestQuery.minDate)
+        }
+    }
+
+    if (requestQuery.maxDate) {
+        ordersQuery.date = {
+            $lte: Date(requestQuery.maxDate)
+        }
+    }
+
+    const orders = await datasources.orders.find(ordersQuery);
     const newOrders = [];
 
     for(let i = 0; i < orders.length; i++) {
         const currentOrder = orders[i];
-        const orderDetails = await datasources.orderDetails.find({order_id: currentOrder._id});
+        const orderDetails =
+            await datasources.orderDetails.find({order_id: currentOrder._id});
 
         newOrders.push({
             _id: currentOrder._id,
@@ -23,6 +71,11 @@ controller.get = async (request) => {
     return newOrders;
 };
 
+/**
+ * Obtiene un pedido dado el identificador
+ * @param request Objeto que contiene el identificado de pedido
+ * @returns El pedido
+ */
 controller.getOne = async (request) => {
     const order = await datasources.orders.findOne({_id: request.params.id});
 
@@ -39,6 +92,15 @@ controller.getOne = async (request) => {
     };
 };
 
+/**
+ * Crea un nuevo pedido
+ * @param request Objecto que contiene el cuerpo del pedido.
+ *                Debe contener un objecto body que posee
+ *                product_ids o los identificadores de los productos,
+ *                junto con la cantidad determinada del producto, que
+ *                por defecto es 1.
+ * @returns El pedido creado
+ */
 controller.create = async (request) => {
     const newOrder = new datasources.orders({date: Date.now()});
     const savedOrder = await newOrder.save();
@@ -62,6 +124,12 @@ controller.create = async (request) => {
     }
 };
 
+/**
+ * Actualiza un pedido
+ * @param request Objeto que contiene los campos a actualizar.
+ *                Debe contener los ids de los nuevos productos a agregar
+ * @returns El objeto actualizado
+ */
 controller.update = async (request) => {
     const order = await datasources.orders.findOne({_id: request.params.id});
 
@@ -91,6 +159,11 @@ controller.update = async (request) => {
     }
 };
 
+/**
+ * Elimina el pedido dado un identificador
+ * @param request Objeto que contiene el identificador
+ * @returns El pedido eliminado
+ */
 controller.delete = async (request) => {
     const order = await controller.getOne(request);
     await datasources.orders.deleteOne({_id: request.params.id});
